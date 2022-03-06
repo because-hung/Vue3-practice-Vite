644d5bb52cdba4614d448f01945bfcb61413bade
Becasuse-hung <4a33a006@stust.edu.tw>
2022-03-07 00:59:55 +0800

    work 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref } from "vue";
+import { computed, onUpdated, reactive, ref, watch } from "vue";
 
 defineProps({
   msg: String,
@@ -59,31 +59,165 @@ const getStar = function (item) {
   console.log(filterData);
 };
 const cancelStar = function (item) {
-
-    const found = filterData.find((el) => {
+  const found = filterData.find((el) => {
     return el.id === item.id;
   });
 
   if (found) {
-   filterData.splice(0, 1);
+    filterData.splice(0, 1);
     localStorage.setItem("NumData", JSON.stringify(filterData));
   }
 
   console.log(filterData);
 };
+
+//work obj array
+
+let foodData = reactive([
+  {
+    name: "tom17",
+    foodColumn: [
+      {
+        country: "USA",
+        id: "01",
+        food: [
+          {
+            cookie: "straw",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "bay12",
+    foodColumn: [
+      {
+        country: "CANA",
+        id: "02",
+        food: [
+          {
+            cookie: "straw",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Kom",
+    foodColumn: [
+      {
+        country: "lorea",
+        id: "03",
+        food: [
+          {
+            cookie: "straw",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "pp",
+    foodColumn: [
+      {
+        country: "USAaaa",
+        id: "04",
+        food: [
+          {
+            cookie: "straw",
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+//work restart 看這裡
+const filtered = ["K", "pp", "bay", "leon"];
+
+const objfilter = Object.entries(foodData);
+
+
+  const filteredData = objfilter.filter((it) => {  //跑左邊比對
+    for (let i = 0; i < filtered.length; i++) {
+      if (it[1].name.includes(filtered[i])) {  //陣列比對 跑右邊
+        console.log("123");
+        return it;  //回傳值
+      }
+    }
+
+  });
+
+ 
+  console.log("filtered", filteredData);
+
+
+
+
+// console.log('data', objfilterW);
+
+// for(let i =0;i<objfilter.length;i++){
+//   for(let j=0;j<=filtered.length;j++){
+//     if(objfilterW[i][1].includes(filtered[j])){
+//       deta.push(objfilterW[i][1])
+//         console.log('for deta', deta)
+
+//     }
+//      console.log('i',i)
+
+//   }
+// }
+
+// console.log('deta', deta);
+
+console.log(foodData);
+// let deta = reactive(foodData)
+// console.log('default', deta)
+let status = ref(0)
+const changeTem = function () {
+//   // deta = reactive(filteredData)
+//   // console.log('change', deta)
+//   const filteredData = objfilter.filter((it) => {
+//     for (let i = 0; i < filtered.length; i++) {
+//       if (it[1].name.includes(filtered[i])) {
+//         console.log("123");
+//         return it;
+//       }
+//     }
+
+//     // return it[1].name ==  filtered[index]
+//   });
+
+//   // for 迴圈  判斷  index 值 change
+//   console.log("filtered", filteredData);
+//   // foodData = reactive(filteredData);
+status.value = 1
+
+};
+ console.log(status)
+
+let arrayA = reactive(["1", "2", "3", "4", "5"])
+let arrayB = reactive(["5", "6", "7", "8", "9"])
+
+
+
+
+
 </script>
 
 <template>
-  <h1 class="text-info">test 釘選</h1>
+  <button @click="changeTem">123</button>
+
   <div class="star">
     <div class="row">
-      <div class="col-3 p-3" v-for="(item, i) in filterData" :key="i">
+      <div class="col-3 p-3" v-for="(item, i) in arrayA" :key="i">
         <div class="card">
           <img src="" class="card-img-top" alt="..." />
           <div class="card-body">
             <div class="r d-flex justify-content-around mb-3">
-              <h5 class="card-title">Card title + {{ item.user }}</h5>
-              <button @click="cancelStar(item)">{{ item.id }}</button>
+              <h5 class="card-title">Card title + {{ item }}</h5>
+              <!-- <h3>{{ item.foodColumn[0].country }}</h3> -->
+              <button @click="cancelStar(item)"></button>
             </div>
 
             <p class="card-text">
@@ -118,6 +252,13 @@ const cancelStar = function (item) {
       </div>
     </div>
   </div>
+
+  <!-- <div class="div">
+    <h1 class="text-info">test 釘選</h1>
+    <ul v-for="(item ,i)in arrayA" :key="i">
+      <li>{{ item }}</li>
+    </ul>
+  </div> -->
 </template>
 
 <style scoped>
